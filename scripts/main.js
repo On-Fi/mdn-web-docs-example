823230f5b8ac7dc7b19b097460fc51f33ed3fbca
onur fistikci <onurfistikci@mail.de>
2023-12-29 17:45:50 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,11 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
 // This function alert is built into the browser.
-alert("Welcome!");
+//alert("Welcome!");
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
 
 // A semicolon at the end of a line indicates where a statement ends.
-let myVariable = "dob";
+//let myVariable = "dob";
